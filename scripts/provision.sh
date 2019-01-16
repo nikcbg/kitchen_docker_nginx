65ce02ab394420010d61d2d44849c8c6e3360f0d
nikcbg <43785382+nikcbg@users.noreply.github.com>
2019-01-16 10:41:02 +0200

    Update provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -11,3 +11,8 @@ which packer || {
   cd /usr/local/bin
   curl -sSL https://raw.githubusercontent.com/kikitux/download-hashicorp-tools/master/download-packer.rb | sudo ruby
 }
+
+# Install dependencies for kitchen
+sudo apt-get update
+sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev 
+sudo apt-get install -y libncurses5-dev libffi-dev libgdbm3 libgdbm-dev ruby-dev
